a3a57a40ae01a67020a0ecb51f68fc32476bdb62
Sebas1020 <juansebascan1212@gmail.com>
2025-02-25 18:36:48 -0500

    crea los componentes base para el poryecto

---
+++ src/app/pages/private/usere/usere.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usere',
+  imports: [],
+  templateUrl: './usere.component.html',
+  styleUrl: './usere.component.css'
+})
+export class UsereComponent {
+
+}
