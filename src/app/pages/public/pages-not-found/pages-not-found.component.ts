1d56b202ad0c17c7f29ea5772a12297260d16f75
Sebas1020 <juansebascan1212@gmail.com>
2025-02-25 19:34:41 -0500

    Cree las rutas y el logo al igual que los texto

---
+++ src/app/pages/public/pages-not-found/pages-not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-not-found',
+  imports: [],
+  templateUrl: './pages-not-found.component.html',
+  styleUrl: './pages-not-found.component.css'
+})
+export class PagesNotFoundComponent {
+
+}
