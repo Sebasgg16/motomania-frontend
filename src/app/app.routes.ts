bd0f19d4dea90ab90b8fbced0036de33a12117af
Sebas1020 <juansebascan1212@gmail.com>
2025-02-25 21:05:32 -0500

    cree categorias, servicios, citas, productos, repuestos, en private y le puse las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,23 @@ import { CreditsComponent } from "./pages/public/credits/credits.component";
 import { UsersComponent } from "./pages/private/users/users.component";
 import { PagesNotFoundComponent } from "./pages/public/pages-not-found/pages-not-found.component";
 import { DashboardComponent } from "./pages/private/dashboard/dashboard.component";
+import { CategoriasComponent } from "./pages/private/categorias/categorias.component";
+import { ProductosComponent } from "./pages/private/productos/productos.component";
+import { CitasComponent } from "./pages/private/citas/citas.component";
+import { RepuestosComponent } from "./pages/private/repuestos/repuestos.component";
+import { ServiciosComponent } from "./pages/private/servicios/servicios.component";
+
+
+
 
 
 
 export const routes : Routes = [
+    {path : 'categorias', component: CategoriasComponent},
+    {path: 'productos', component: ProductosComponent},
+    {path: 'citas', component: CitasComponent},
+    {path: 'repuestos', component: RepuestosComponent},
+    {path: 'servicios', component: ServiciosComponent},
     {path : 'home', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'login', component: RegisterComponent},
