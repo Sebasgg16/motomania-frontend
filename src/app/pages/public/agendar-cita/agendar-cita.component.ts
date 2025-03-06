4e0ee5ee913fd8d3cbffad662f251523a2f92e42
Sebas1020 <juansebascan1212@gmail.com>
2025-03-05 22:56:08 -0500

    solo fue resolver un conflicto

--- src/app/pages/public/agendar-cita/agendar-cita.component.ts
+++ src/app/pages/public/agendar-cita/agendar-cita.component.ts
@@ -11,10 +11,13 @@ export class AgendarCitaComponent {
   formData!:FormGroup
   constructor(){
     this.formData = new FormGroup({
-      agregarprecio: new FormControl('', [Validators.required]),
-      agregarservicio: new FormControl('', [Validators.required]),
-      agregardescripcion: new FormControl ('', [Validators.required]),
-      agregarimagen: new FormControl ('', [Validators.required])
+      // name: new FormControl('', [Validators.required]),
+      // description: new FormControl('', [Validators.required]),
+      // price: new FormControl ('', [Validators.required]),
+      // stock: new FormControl ('', [Validators.required]),
+      // urlImage: new FormControl('', [Validators.required]),
+      // categorias: new FormControl('', [Validators.required]),
+      // state: new FormControl ('', [Validators.required]),
     })
 
   }
