62f790df99916e0580e5f0be45ddd7cc7c2d6aac
Sebas1020 <juansebascan1212@gmail.com>
2025-03-06 23:56:09 -0500

    cree los formularios de editar repuestos y servicios al igual que pase el header y el footer y el home y quede en proceso para subir la pantalla de servicios

--- src/app/pages/private/editar-servicio/editar-servicio.component.ts
+++ src/app/pages/private/editar-servicio/editar-servicio.component.ts
@@ -1,20 +1,20 @@
-  import { Component, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-crear-servicio',
+  selector: 'app-editar-servicio',
   imports: [ReactiveFormsModule],
-  templateUrl: './crear-servicio.component.html',
-  styleUrl: './crear-servicio.component.css'
+  templateUrl: './editar-servicio.component.html',
+  styleUrl: './editar-servicio.component.css'
 })
-export class CrearServicioComponent {
+export class EditarServicioComponent {
   formData!:FormGroup
 
   constructor(){
     this.formData = new FormGroup({
-      urlImagen: new FormControl('',[Validators.required]),
-      price: new FormControl(0, [Validators.required]),
+      urlImagen: new FormControl ('', [Validators.required]),
       name: new FormControl('', [Validators.required]),
+      price: new FormControl(0, [Validators.required]),
       description: new FormControl ('', [Validators.required]),
     })
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { UrlCodec } from '@angular/common/upgrade';
 import { Injectable } from '@angular/core';
+import { Observable, observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,6 @@ export class AuthService {
   registerUser(newUser: any) {
 
     this.http.post('http://localhost:3000/api/auth/register', newUser)
+  
   }
 }
